1a6d001710b58ca99d903820df909b8d5ab4c0d8
Thomas Nigro <tn@thomasnigro.fr>
2015-08-13 11:30:19 +0200

    Code cosmetic

--- ScrollWatcher/ItemsControlHelper.cs
+++ ScrollWatcher/ItemsControlHelper.cs
@@ -5,10 +5,10 @@ namespace ScrollWatcher
 {
     public static class ItemsControlHelper
     {
-        private static IScrollWatcher _ScrollWatcher;
-        public static ScrollViewer RegisterScrollViewer(ItemsControl selector, IScrollWatcher ScrollWatcher)
+        private static IScrollWatcher _scrollWatcher;
+        public static ScrollViewer RegisterScrollViewer(ItemsControl selector, IScrollWatcher scrollWatcher)
         {
-            _ScrollWatcher = ScrollWatcher;
+            _scrollWatcher = scrollWatcher;
             ScrollViewer sV = null;
             sV = selector.GetFirstDescendantOfType<ScrollViewer>();
             sV.ViewChanging += ScrollViewerOnViewChanging;
@@ -18,29 +18,29 @@ namespace ScrollWatcher
         public static void ScrollViewerOnViewChanging(object sender, ScrollViewerViewChangingEventArgs scrollViewerViewChangingEventArgs)
         {
             var voffset = scrollViewerViewChangingEventArgs.NextView.VerticalOffset;
-            if (voffset < 50 || voffset > (_ScrollWatcher.ScrollViewer.ScrollableHeight - 50)) return;
+            if (voffset < 50 || voffset > (_scrollWatcher.ScrollViewer.ScrollableHeight - 50)) return;
 
-            if (voffset > _ScrollWatcher.Scroll + 30)
+            if (voffset > _scrollWatcher.Scroll + 30)
             {
                 // Scrolling to bottom
-                if (_ScrollWatcher.ScrollDirection != ScrollDirection.Bottom)
+                if (_scrollWatcher.ScrollDirection != ScrollDirection.Bottom)
                 {
-                    _ScrollWatcher.ScrollDirection = ScrollDirection.Bottom;
-                    ScrollingEventArgs e = new ScrollingEventArgs(_ScrollWatcher.ScrollDirection);
-                    _ScrollWatcher.InvokeScrollingEvent(e);
+                    _scrollWatcher.ScrollDirection = ScrollDirection.Bottom;
+                    ScrollingEventArgs e = new ScrollingEventArgs(_scrollWatcher.ScrollDirection);
+                    _scrollWatcher.InvokeScrollingEvent(e);
                 }
-                _ScrollWatcher.Scroll = voffset;
+                _scrollWatcher.Scroll = voffset;
             }
-            else if (voffset < _ScrollWatcher.Scroll - 30)
+            else if (voffset < _scrollWatcher.Scroll - 30)
             {
                 // Scrolling to top
-                if (_ScrollWatcher.ScrollDirection != ScrollDirection.Top)
+                if (_scrollWatcher.ScrollDirection != ScrollDirection.Top)
                 {
-                    _ScrollWatcher.ScrollDirection = ScrollDirection.Top;
-                    ScrollingEventArgs e = new ScrollingEventArgs(_ScrollWatcher.ScrollDirection);
-                    _ScrollWatcher.InvokeScrollingEvent(e);
+                    _scrollWatcher.ScrollDirection = ScrollDirection.Top;
+                    ScrollingEventArgs e = new ScrollingEventArgs(_scrollWatcher.ScrollDirection);
+                    _scrollWatcher.InvokeScrollingEvent(e);
                 }
-                _ScrollWatcher.Scroll = voffset;
+                _scrollWatcher.Scroll = voffset;
             }
         }
     }
